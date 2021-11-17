b81aa18779a3af7fffc72e91b375ddd3963125af
Tim Washington <taw@tehmwashington.com>
2021-11-17 14:15:17 -0600

    update push

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../server/index.js'));
+});
+
+app.use('/js', express.static(path.join(__dirname, 'server/index.js')));
+
+
 
 app.listen(port, () => {
     console.log(`This app is lit on server ${port}`)
