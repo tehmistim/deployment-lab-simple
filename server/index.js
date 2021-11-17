3f60a9046a960d0dabc61dc9afdad4471114f83a
Tim Washington <taw@tehmwashington.com>
2021-11-17 15:03:48 -0600

    update push

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../server/index.js'));
 });
 
+app.get('/images/header.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../images/header.jpg'));
+})
+
 app.use('/server/index.js', express.static(path.join(__dirname, '../server/index.js')));
 
 
